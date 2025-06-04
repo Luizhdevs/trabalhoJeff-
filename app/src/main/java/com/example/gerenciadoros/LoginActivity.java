ae587045b330b247e1d9d1f2e807ea92d01d10e8
Luiz Henrique Campos Ferreira <138630678+Luizhdevs@users.noreply.github.com>
2025-06-04 12:09:39 -0300

    Update LoginActivity.java
    comentarios

--- app/src/main/java/com/example/gerenciadoros/LoginActivity.java
+++ app/src/main/java/com/example/gerenciadoros/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 
 public class LoginActivity extends AppCompatActivity {
+    //declarando os elementos que vai usar no código
     EditText edtEmail, edtSenha;
     Button btnLogin;
     TextView txtCadastrar;
@@ -19,15 +20,19 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Carrega a tela com o layout activity_login.xml.
         setContentView(R.layout.activity_login);
 
+        //Conecta os elementos do XML aos objetos no código.
         edtEmail = findViewById(R.id.edtEmail);
         edtSenha = findViewById(R.id.edtSenha);
         btnLogin = findViewById(R.id.btnLogin);
         txtCadastrar = findViewById(R.id.txtCadastrar);
 
         btnLogin.setOnClickListener(v -> {
+            //Acessa as informações salvas em SharedPreferences (como se fosse um pequeno banco de dados local).
             SharedPreferences prefs = getSharedPreferences("usuarios", MODE_PRIVATE);
+            //Compara o email e a senha digitados com os valores salvos.
             String emailSalvo = prefs.getString("email", "");
             String senhaSalva = prefs.getString("senha", "");
 
@@ -36,14 +41,17 @@ public class LoginActivity extends AppCompatActivity {
 
 
             if(email.equals(emailSalvo) && senha.equals(senhaSalva)) {
+                //Se forem iguais, abre a próxima tela MainMenuActivity.
                 startActivity(new Intent(this, MainMenuActivity.class));
                 finish();
             } else {
+                //Se forem diferentes, mostra um aviso "Login inválido".
                 Toast.makeText(this, "Login inválido", Toast.LENGTH_SHORT).show();
             }
         });
 
         txtCadastrar.setOnClickListener(v -> {
+            //Ao clicar no texto, o usuário vai para a tela de cadastro (RegisterActivity).
             startActivity(new Intent(this, RegisterActivity.class));
         });
     }
